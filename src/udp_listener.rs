84b7515816002a86e16bac26b779c3c2791563d2
Frank Denis <github@pureftpd.org>
2017-03-26 21:22:39 +0200

    up

--- src/udp_listener.rs
+++ src/udp_listener.rs
@@ -19,52 +19,86 @@ use varz::Varz;
 use super::{DNS_MAX_UDP_SIZE, DNS_QUERY_MIN_SIZE, DNS_QUERY_MAX_SIZE};
 
 pub struct UdpListener {
-    event_loop: Core,
-    socket: UdpSocket,
+    event_loop: Option<Core>,
+    socket: Option<UdpSocket>,
     resolver_tx: Sender<ClientQuery>,
-    service_ready_tx: mpsc::SyncSender<u8>,
+    service_ready_tx: Option<mpsc::SyncSender<u8>>,
     cache: Cache,
     varz: Arc<Varz>,
+    packet_buf: Option<Vec<u8>>,
 }
 
-#[derive(Debug)]
-struct UdpClientSession {
-    packet: Option<Vec<u8>>,
-    socket: Option<UdpSocket>,
-}
-
-impl UdpClientSession {
-    fn new(socket: UdpSocket) -> Self {
-        UdpClientSession {
-            packet: Some(vec![0u8; DNS_MAX_UDP_SIZE]),
-            socket: Some(socket),
-        }
-    }
-
+impl UdpListener {
     fn process(mut self) -> impl Future<Item = Loop<Self, Self>, Error = io::Error> {
         self.socket
             .take()
             .unwrap()
-            .recv_dgram(self.packet.take().unwrap())
-            .map(move |(socket, packet, count, client_addr)| (socket, packet))
-            .and_then(move |(socket, packet)| {
-                          println!("received");
-                          self.packet = Some(packet);
-                          self.socket = Some(socket);
-                          Ok(Loop::Continue(self))
-                      })
+            .recv_dgram(self.packet_buf.take().unwrap())
+            .and_then(move |(socket, packet_buf, count, client_addr)| {
+                println!("received");
+                self.socket = Some(socket);
+                self.packet_buf = Some(packet_buf);
+                self.varz.client_queries_udp.inc();
+                if count < DNS_QUERY_MIN_SIZE || count > DNS_QUERY_MAX_SIZE {
+                    info!("Short query using UDP");
+                    self.varz.client_queries_errors.inc();
+                    return Ok(Loop::Continue(self));
+                }
+                let normalized_question =
+                    match dns::normalize(&self.packet_buf.as_ref().unwrap()[..count], true) {
+                        Ok(normalized_question) => normalized_question,
+                        Err(e) => {
+                            debug!("Error while parsing the question: {}", e);
+                            self.varz.client_queries_errors.inc();
+                            return Ok(Loop::Continue(self));
+                        }
+                    };
+                let cache_entry = self.cache.get2(&normalized_question);
+                if let Some(mut cache_entry) = cache_entry {
+                    if !cache_entry.is_expired() {
+                        self.varz.client_queries_cached.inc();
+                        if cache_entry.packet.len() > normalized_question.payload_size as usize {
+                            debug!("cached, but has to be truncated");
+                            let packet = dns::build_tc_packet(&normalized_question).unwrap();
+                            let _ = self.socket
+                                .as_ref()
+                                .unwrap()
+                                .send_to(&packet, &client_addr);
+                            return Ok(Loop::Continue(self));
+                        }
+                        debug!("cached");
+                        dns::set_tid(&mut cache_entry.packet, normalized_question.tid);
+                        dns::overwrite_qname(&mut cache_entry.packet, &normalized_question.qname);
+                        let _ = self.socket
+                            .as_ref()
+                            .unwrap()
+                            .send_to(&cache_entry.packet, &client_addr);
+                        return Ok(Loop::Continue(self));
+                    }
+                    debug!("expired");
+                    self.varz.client_queries_expired.inc();
+                }
+                let client_query = ClientQuery {
+                    proto: ClientQueryProtocol::UDP,
+                    client_addr: Some(client_addr),
+                    tcpclient_tx: None,
+                    normalized_question: normalized_question,
+                    ts: Instant::recent(),
+                };
+                //self.resolver_tx.send(client_query).and_then(|resolver_tx| Ok(Loop::Continue(self)))
+                Ok(Loop::Continue(self))
+            })
     }
-}
 
-impl UdpListener {
     fn run(mut self) -> io::Result<()> {
         debug!("udp listener socket={:?}", self.socket);
-        let stream = loop_fn(UdpClientSession::new(self.socket),
-                             |session| session.process());
-        self.event_loop.handle().spawn(stream.map_err(|_| {}).map(|_| {}));
-        self.service_ready_tx.send(0).unwrap();
+        let mut event_loop = self.event_loop.take().unwrap();
+        let service_ready_tx = self.service_ready_tx.take().unwrap();
+        let stream = loop_fn(self, |session| session.process());
+        event_loop.handle().spawn(stream.map_err(|_| {}).map(|_| {}));
+        service_ready_tx.send(0).unwrap();
         loop {
-            self.event_loop.turn(None)
+            event_loop.turn(None)
         }
     }
 
@@ -83,12 +117,13 @@ impl UdpListener {
                 let udp_socket = UdpSocket::from_socket(net_udp_socket, &event_loop.handle())
                     .expect("Unable to use a local UDP listener");
                 let udp_listener = UdpListener {
-                    event_loop,
-                    socket: udp_socket,
+                    event_loop: Some(event_loop),
+                    socket: Some(udp_socket),
                     resolver_tx,
-                    service_ready_tx,
+                    service_ready_tx: Some(service_ready_tx),
                     cache,
                     varz,
+                    packet_buf: Some(vec![0u8; DNS_MAX_UDP_SIZE]),
                 };
                 udp_listener.run().expect("Unable to spawn a UDP listener");
             })
